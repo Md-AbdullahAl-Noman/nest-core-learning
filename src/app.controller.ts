d1316667d5326471cb8022ee2e7dad805f7959b9
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2025-04-26 00:57:21 +0600

    Define routes for the AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller("/app")// This is the controller for the app module as whole .this will handle all the routes that are inside this controller
 export class AppController {
-  @Get()
+  @Get("/hello")//manage the different routes and the different methods that can be used to access them
   getHello(): string {
     return "Hello  bro!";
   }
